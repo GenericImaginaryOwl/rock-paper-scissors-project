115fd6b633bfee855e21f8079efdbc71f6073d1c
Generic Owl <242839885+GenericImaginaryOwl@users.noreply.github.com>
2025-11-28 19:15:29 +0400

    Add logic for computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 console.log("Hello");
+
+function getComputerChoice() {
+
+    switch( Math.floor(Math.random() * 3)) {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
