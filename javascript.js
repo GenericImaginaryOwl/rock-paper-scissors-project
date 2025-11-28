53f5492a61fbcf8ff87401a1717ef4a8f75df14f
Generic Owl <242839885+GenericImaginaryOwl@users.noreply.github.com>
2025-11-28 20:28:59 +0400

    Add logic to play a single round of the game

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 console.log("Hello");
 
-function getComputerChoice() {
+let humanScore = 0;
+let computerScore = 0;
 
-    switch( Math.floor(Math.random() * 3)) {
+function getComputerChoice() {
+    switch(Math.floor(Math.random() * 3)) {
         case 0: return "Rock";
         case 1: return "Paper";
         case 2: return "Scissors";
@@ -14,5 +16,27 @@ function getHumanChoice(){
     return prompt("Please enter Rock Paper or Scissors");
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        console.log("This round ended in a draw");
+        humanScore += 0.5;
+        computerScore += 0.5;    
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissors"
+          || humanChoice === "Paper" && computerChoice === "Rock"
+          || humanChoice === "Scissors" && computerChoice == "Paper") {   
+        console.log(`You won this round: ${humanChoice} beats ${computerChoice}.`);
+        humanChoice += 1;
+    }
+    else {
+        //the list of possible options besides player losing is exhausted by this point
+        console.log(`You lost this round: ${humanChoice} is beaten by ${computerChoice}.`)
+        computerScore +=1;
+    }
+    
+    
+}
+playRound(getHumanChoice(), getComputerChoice());
+
+//console.log(getComputerChoice());
+//console.log(getHumanChoice());
