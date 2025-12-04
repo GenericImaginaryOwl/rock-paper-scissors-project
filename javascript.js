36288b179e6672bddada44ed12a0850710c717fe
Generic Owl <242839885+GenericImaginaryOwl@users.noreply.github.com>
2025-12-04 04:19:17 +0400

    Add UI

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
-console.log("Hello");
+let humanScore = 0;
+let computerScore = 0;
 
+const optionsContainer = document.querySelector(".options-container"); 
+const resultsContainer = document.querySelector(".results-container");
+optionsContainer.addEventListener("click", playRound);
 
 
 function getComputerChoice() {
@@ -10,43 +14,31 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice(){
-    //assuming the input is almost always valid here
-    humanInput = prompt("Please enter Rock Paper or Scissors");
-    return humanInput.toLowerCase().replace(humanInput.charAt(0), humanInput.charAt(0).toUpperCase());
+function endGame() {
+    resultsContainer.textContent = `The game has ended.\nFinal score: You - ${humanScore}. Computer - ${computerScore}.`
+    optionsContainer.removeEventListener("click", playRound);
 }
 
+function playRound(event) {
+    const humanChoice = event.target.textContent;
+    const computerChoice = getComputerChoice();
 
-
-function playGame(){ 
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
     if(humanChoice === computerChoice) {
-        console.log("This round ended in a draw");
-        humanScore += 0.5;
-        computerScore += 0.5;    
+        resultsContainer.textContent = "This round ended in a draw.";   
     }
     else if (humanChoice === "Rock" && computerChoice === "Scissors"
-          || humanChoice === "Paper" && computerChoice === "Rock"
-          || humanChoice === "Scissors" && computerChoice == "Paper") {   
-        console.log(`You won this round: ${humanChoice} beats ${computerChoice}.`);
-        humanChoice += 1;
+        || humanChoice === "Paper" && computerChoice === "Rock"
+        || humanChoice === "Scissors" && computerChoice == "Paper") {   
+        resultsContainer.textContent = `You won this round: ${humanChoice} beats ${computerChoice}.`;
+        humanScore += 1;
     }
     else {
         //the list of possible options besides player losing (and invalid input) is exhausted by this point
-        console.log(`You lost this round: ${humanChoice} is beaten by ${computerChoice}.`);
+        resultsContainer.textContent = `You lost this round: ${humanChoice} is beaten by ${computerChoice}.`;
         computerScore += 1;
     }
-}
 
-    for (let round = 1; round <= 5; round++){
-        console.log(`Round ${round}`);
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log('');
+    if(humanScore >= 5 || computerScore >= 5) {
+        endGame();
     }
-    console.log(`The game has ended.\nFinal score: You - ${humanScore}. Computer - ${computerScore}.`);
 }
-
-playGame();
